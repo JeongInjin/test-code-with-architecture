47dd4316aec86bc9260c4dbb9df9917970532d1f
injinjeong <snowwwwww@naver.com>
2023-07-05 13:12:18 +0900

    first commit

---
+++ src/main/java/com/me/injin/testcodewithwitharchitecture/TestCodeWithWithArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.me.injin.testcodewithwitharchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodeWithWithArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCodeWithWithArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/me/injin/testcodewithwitharchitecture/TestCodeWithWithArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.injin.testcodewithwitharchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodeWithWithArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

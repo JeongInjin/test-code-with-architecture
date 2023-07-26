a29c9273ec3a8d76a697f00863e38d86957eabd1
injinjeong <snowwwwww@naver.com>
2023-07-26 10:13:08 +0900

    UserResponseTest 추가

--- src/test/java/com/me/injin/testcodewithwitharchitecture/user/controller/response/UserResponseTest.java
+++ src/test/java/com/me/injin/testcodewithwitharchitecture/user/controller/response/UserResponseTest.java
@@ -1,15 +1,33 @@
 package com.me.injin.testcodewithwitharchitecture.user.controller.response;
 
+import com.me.injin.testcodewithwitharchitecture.user.domain.User;
+import com.me.injin.testcodewithwitharchitecture.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class UserResponseTest {
 
     @Test
     public void User의_응답을_생성할_수_있다() throws Exception {
-        //given
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("injin.dev@gmail.com")
+                .nickname("injin")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .lastLoginAt(100L)
+                .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+                .build();
 
-        //when
+        // when
+        UserResponse userResponse = UserResponse.from(user);
 
-        //then
+        // then
+        assertThat(userResponse.getId()).isEqualTo(1);
+        assertThat(userResponse.getEmail()).isEqualTo("injin.dev@gmail.com");
+        assertThat(userResponse.getStatus()).isEqualTo(UserStatus.ACTIVE);
+        assertThat(userResponse.getLastLoginAt()).isEqualTo(100L);
     }
 }
